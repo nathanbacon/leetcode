fb2301ec799c1e3eb9eb7198f72354384ed223c6
Nathan Gelman <nathanjlg@gmail.com>
2023-03-04 21:55:41 -0800

    where will the ball fall

---
+++ 1706/README.md
@@ -0,0 +1,1 @@
+Dynamic programming approach with emphasis on concise and clear logic and low memory consumption
---
+++ 1706/WhereWillTheBallFall.cs
@@ -0,0 +1,53 @@
+﻿namespace WhereWillTheBallFall;
+
+public class Solution
+{
+  public int[] FindBall(int[][] grid)
+  {
+    int[] origin = new int[grid[0].Length];
+    for (int i = 0; i < origin.Length; i++)
+      origin[i] = i;
+
+    foreach (int[] row in grid)
+    {
+      int[] next = new int[origin.Length];
+      for (int i = 0; i < origin.Length; i++)
+        next[i] = -1;
+
+      for (int i = 0; i < row.Length; i++)
+      {
+        if (row[i] == 1)
+        {
+          if (i + 1 == row.Length) continue;
+          if (row[i + 1] == -1) continue;
+
+          next[i + 1] = origin[i];
+        }
+        else
+        {
+          if (i == 0) continue;
+          if (row[i - 1] == 1) continue;
+          next[i - 1] = origin[i];
+        }
+      }
+
+      origin = next;
+    }
+
+    int[] output = new int[origin.Length];
+
+    for (int i = 0; i < output.Length; i++)
+    {
+      output[i] = -1;
+    }
+
+    for (int i = 0; i < origin.Length; i++)
+    {
+      if (origin[i] == -1) continue;
+
+      output[origin[i]] = i;
+    }
+
+    return output;
+  }
+}
